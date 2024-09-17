b544b3b40c98c061d8f5315302d5fc905fca2f44
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2024-09-17 12:44:39 +0200

    readyy

--- src/components/MainBlock/MainBlockText.jsx
+++ src/components/MainBlock/MainBlockText.jsx
@@ -1,30 +1,22 @@
+
+
 import MainTitle from "@/components/ui/MainTitle";
 import Text from "@/components/ui/Text";
 import ButtonBlue from '@/components/ui/buttons/ButtonBlue';
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const MainBlockText = ({
     text,
     buttonText,
     title
 }) => {
-    const navigate = useNavigate();
-
-    const handleClick = (e) => {
-        e.preventDefault();
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-        setTimeout(() => {
-            navigate("/myprofile/profile");
-        }, 300);
-    };
-
     return (
-        <div onClick={handleClick}>
+        <Link to="myprofile/profile">
             <MainTitle title={title} styles={" mb-4"} />
             <Text styles={" mb-6"} text={text} />
             <ButtonBlue text={buttonText} />
-        </div>
+        </Link>
     )
 }
 
-export default MainBlockText;
+export default MainBlockText
