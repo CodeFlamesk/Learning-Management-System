98e335367e5b924bb2ba269ad025fce225559163
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2024-09-20 15:32:09 +0200

    opt 13

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Container from '@components/ui/Container';
 import Categories from '@components/Home/categories/Categories';
 import MainSlider from '@components/MainSlider/MainSlider';
 import CoursesNumber from '@components/CoursesNumber/CoursesNumber';
-import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
 
 const Courses = lazy(() => import('@components/Courses/Courses'));
 const Instructors = lazy(() => import('@components/Instructors/Instructors'));
@@ -20,14 +20,16 @@ const Home = () => {
 
             <Container >
                 <Categories />
-                <Courses />
-                <Instructors />
+                <Suspense><Courses /></Suspense>
+                <Suspense><Instructors /></Suspense>
+
             </Container>
 
-            <div className="max-w-full mx-auto px-5 sm:px-8 bg-sl-main pb-7 sm:pb-16"><CommentsSlider /> </div>
+            <div className="max-w-full mx-auto px-5 sm:px-8 bg-sl-main pb-7 sm:pb-16"><Suspense><CommentsSlider /></Suspense>  </div>
             <Container >
-                <BecomeInstructor />
-                <CheckoutCourses />
+                <Suspense><BecomeInstructor /></Suspense>
+                <Suspense><CheckoutCourses /></Suspense>
+
             </Container>
         </>
     )
